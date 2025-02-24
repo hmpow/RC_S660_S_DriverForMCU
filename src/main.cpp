234dfdac140426736cdc79d1ab7e4f12376385c7
hmpow <67511807+hmpow@users.noreply.github.com>
2025-02-24 21:00:22 +0900

    PIO vector使えるっぽい！！！

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,32 @@
 #include <Arduino.h>
 #include "./rcs660s/rcs660s_apdu.h"
+#include <vector>
 
 //マニュアル指定定数
 const uint8_t FULL_COMMAND_GetFirmWareVersion[] = {0x00, 0x00, 0xFF, 0x00, 0x0E, 0xF2, 0x6B, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0x56, 0x00, 0x00, 0x3C, 0x00};
 
+std::vector <uint8_t> test; //これ使えればmalloc使わずに済む
 
 void setup() {
   // put your setup code here, to run once:
   setupSerial();
+
+  test.push_back(0x00);
+  test.push_back(0x01);
+  test.push_back(0x02);
+
+
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   
+  for (size_t i = 0; i < test.size(); i++)
+  {
+    debugPrintHex(test[i]);
+  }
+
   //レシーバ初期化
   uart_receiver_init();
 
